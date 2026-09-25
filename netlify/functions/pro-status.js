f269850155eacbb5027e0046dc5cfb29d3aeffe3
earnpathofficial <0903660ab@gmail.com>
2026-09-25 08:00:23 +0100

    Add Pro status Netlify function
    Add a simple Netlify Function to test the ImageTools Pro backend connection.

---
+++ netlify/functions/pro-status.js
@@ -0,0 +1,12 @@
+exports.handler = async function () {
+  return {
+    statusCode: 200,
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      status: "online",
+      service: "ImageTools Pro Backend"
+    })
+  };
+};
